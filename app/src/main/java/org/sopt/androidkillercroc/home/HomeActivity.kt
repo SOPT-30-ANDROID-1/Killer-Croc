f53537213f9ca4b7fa629304be4a328dbbe803b6
Leekangmin <kkk5474096@naver.com>
2022-04-09 19:19:57 +0900

    [feature] HomeActivity

---
+++ app/src/main/java/org/sopt/androidkillercroc/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.androidkillercroc.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.androidkillercroc.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
